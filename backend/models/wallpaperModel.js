e7e008640131bde0fd28c02b1e458dd09cea9110
liro-u <86537787+liro-u@users.noreply.github.com>
2023-03-15 11:48:28 +0100

    backend

---
+++ backend/models/wallpaperModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const wallpaperSchema = new mongoose.Schema({
+    imageLink: {
+        type: String,
+        required: [true, 'Please enter a Link to the wallpaper'],
+        unique: true
+    },
+    artistId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: [true, 'Please enter a title to the wallpaper'],
+    },
+    titleColor: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Wallpaper = mongoose.model('wallpaper', wallpaperSchema);
+
+export default Wallpaper;
---
+++ frontend/src/components/buttons/ButtonLoadMore.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from "react";
+// css
+import "../../css/buttons.css";
+
+const ButtonLoadMore = () => {
+    const [offset, setOffset] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [maxScrollValue, setMaxScrollValue] = useState(0);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener('scroll', function () {
+            var posBarAtm = window.scrollY
+            setHeight(ref.current.offsetHeight)
+            setMaxScrollValue(Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ));
+            // position transition
+            if (posBarAtm + window.innerHeight >= maxScrollValue) {
+                setOffset(0);
+            } else {
+                setOffset(-height);
+            }
+        })    
+    }, [height, maxScrollValue])
+
+    
+
+    return (
+        <div className="loadMoreContainer">
+            <div 
+                className="loadMore primaryColor"
+                ref={ref}
+                style={{
+                    bottom: offset + "px",
+                }}
+            >
+                <p>Load More</p>
+                <span className="material-symbols-outlined arrowDown">expand_more</span>
+            </div>
+        </div>
+        )
+}
+
+export default ButtonLoadMore;

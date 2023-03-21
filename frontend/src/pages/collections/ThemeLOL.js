17de9c20b914e53961b3a1c21a01ab4433c8fc43
axelvuillaume <axelvuillaume@hotmail.fr>
2023-03-21 13:42:48 +0100

    cc

--- frontend/src/pages/collections/ThemeLOL.js
+++ frontend/src/pages/collections/ThemeLOL.js
@@ -3,12 +3,20 @@ import React from "react";
 // components
 import NavbarOffset from "../../components/NavbarOffset";
 
+//css
+import "../../css/ThemeLol.css"
+
 const ThemeLOL = () => {
 
     return (
-        <div>
+
+        <div style={{
+            backgroundImage : `url("/wallpaper/FondImgThemeLol.png")`
+        }}>
+            <div className="flip-Card"></div>
+
             <NavbarOffset />
-            <h1>Ceci est la page de la collection League of legends</h1>
+            <h1>Ceci est la page de la collection League of legends test</h1>
         </div>
     )
 }

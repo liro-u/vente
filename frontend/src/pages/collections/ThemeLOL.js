f2ff446a77b6f5cf8c80314a2153bcba6025fcd6
axelvuillaume <axelvuillaume@hotmail.fr>
2023-03-22 12:48:14 +0100

    vbb

--- frontend/src/pages/collections/ThemeLOL.js
+++ frontend/src/pages/collections/ThemeLOL.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 // components
 import NavbarOffset from "../../components/NavbarOffset";
@@ -6,17 +6,47 @@ import NavbarOffset from "../../components/NavbarOffset";
 //css
 import "../../css/ThemeLol.css"
 
-const ThemeLOL = () => {
+const ThemeLOL = ({id}) => {
+    const [test, setTest] = useState(null)
+    const container = useRef(null);
+    const [height, setHeight] = useState(0);
+    const TailleImg = useRef(null);
+
+    const handleClick  = () => {
+        container.current.classList.toggle('flipCard');
+        setHeight(TailleImg.current.offsetHeight)
+        console.log(height)
+    };
+
+    useEffect(()=>{
+        const fecthImg = async (id) => {
+            console.log(process.env.REACT_APP_PROXY + '/api/wallpapers/' + id)
+            const response = await fetch(process.env.REACT_APP_PROXY + '/api/wallpapers/' + id);
+            const img = await response.json()
+            setTest(img);
+        }
+        fecthImg("6410dd5ad26154414a108244")
+    }, [])
 
     return (
 
         <div style={{
             backgroundImage : `url("/wallpaper/FondImgThemeLol.png")`
         }}>
-            <div className="flip-Card"></div>
-
             <NavbarOffset />
-            <h1>Ceci est la page de la collection League of legends test</h1>
+
+            <div className="cardContainer" onClick={handleClick}>
+                <div className="card" ref={container} style={{height : height}}>
+                    <div className="flipImg">
+                         {test && <img src={test.imageLink } alt="toto" ref={TailleImg}/>}
+
+                    </div>
+
+                    <div className="flipContenu">
+                        <p> test </p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
